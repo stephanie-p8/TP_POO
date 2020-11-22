4ad90f958a9e2f8efa48032c841459afdadeafc9
Stephanie <stephanie.perafanpatino@gmail.com>
2020-11-22 16:16:00 +0100

    Methods for number and name

--- Tarot/src/Model/Card.java
+++ Tarot/src/Model/Card.java
@@ -1,5 +1,24 @@
 package Model;
 
 public class Card {
+	private int number;
+	private String name;
+	
+	public Card(int number,String name) {
+		this.number=number;
+		this.name=name;
+	}
+	
+	public void addDescription() {
+		
+	}
+	
+	public int getCardNumber() {
+		return number;
+	}
+	
+	public String getCardName() {
+		return name;
+	}
 
 }
