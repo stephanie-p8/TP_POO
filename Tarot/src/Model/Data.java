61ddd079c9b895d9e7e08214166453044c703685
Stephanie <stephanie.perafanpatino@gmail.com>
2020-11-22 16:46:48 +0100

    Contains all the constants of the project

---
+++ Tarot/src/Model/Data.java
@@ -0,0 +1,10 @@
+package Model;
+
+public class Data {
+
+	//Les arcanes majeures
+	public static final String [] ARCANES_MAJEURES= {"Le Mat","Le Bateleur","La Papesse","L’Impératrice","L’Empereur"
+			,"Le Pape","L’Amoureux","Le Chariot","La Justice","L’Hermite","La Roue de Fortune","La Force",
+			"Le Pendu","La carte sans nom","La Tempérance","Le Diable","La Maison Dieu","L’étoile","La Lune",
+			"Le Soleil","Le Jugement","Le Monde"};
+}
