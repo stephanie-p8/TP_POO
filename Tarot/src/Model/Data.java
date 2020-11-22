fef8e49f070dfe50e41b86760378a807943ea4cb
Stephanie <stephanie.perafanpatino@gmail.com>
2020-11-22 17:35:39 +0100

    Add number of major mystery

--- Tarot/src/Model/Data.java
+++ Tarot/src/Model/Data.java
@@ -7,4 +7,6 @@ public class Data {
 			,"Le Pape","L’Amoureux","Le Chariot","La Justice","L’Hermite","La Roue de Fortune","La Force",
 			"Le Pendu","La carte sans nom","La Tempérance","Le Diable","La Maison Dieu","L’étoile","La Lune",
 			"Le Soleil","Le Jugement","Le Monde"};
+	
+	public static final int NB_ARCANES_MAJEURES = 22;
 }
