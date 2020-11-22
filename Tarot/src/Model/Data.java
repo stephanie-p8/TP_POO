7e812554bb9ce78a3bedc9028e71af5477560c5b
Stephanie <stephanie.perafanpatino@gmail.com>
2020-11-22 17:39:45 +0100

    Change name of values

--- Tarot/src/Model/Data.java
+++ Tarot/src/Model/Data.java
@@ -2,11 +2,11 @@ package Model;
 
 public class Data {
 
-	//Les arcanes majeures
-	public static final String [] ARCANES_MAJEURES= {"Le Mat","Le Bateleur","La Papesse","L’Impératrice","L’Empereur"
+	//The major mysteries
+	public static final String [] MAJOR_MYSTERY= {"Le Mat","Le Bateleur","La Papesse","L’Impératrice","L’Empereur"
 			,"Le Pape","L’Amoureux","Le Chariot","La Justice","L’Hermite","La Roue de Fortune","La Force",
 			"Le Pendu","La carte sans nom","La Tempérance","Le Diable","La Maison Dieu","L’étoile","La Lune",
 			"Le Soleil","Le Jugement","Le Monde"};
 	
-	public static final int NB_ARCANES_MAJEURES = 22;
+	public static final int NB_MAJOR_MYSTERY = 22;
 }
