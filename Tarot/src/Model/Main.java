db311a8881a0fc0e58854dfc7fcad0e67942ca00
Stephanie <stephanie.perafanpatino@gmail.com>
2020-11-23 18:40:07 +0100

    Try to create a deck with all the major mystery card and try to remove a card from the deck

--- Tarot/src/Model/Main.java
+++ Tarot/src/Model/Main.java
@@ -1,10 +1,26 @@
 package Model;
 
+import java.util.ArrayList;
+
 public class Main {
 	public static void main(String[]args) {
 		Card c = new Card(4,Data.MAJOR_MYSTERY[3]);
 		System.out.println(c.toString());
 		c.addDescription("Papier, cours, idée, projet, élaboration mentale");
 		System.out.println(c.toString());
+		
+		ArrayList<Card>myCards = new ArrayList<Card>();
+		Deck myDeck = new Deck(myCards);
+		
+		for(int i=0;i<Data.NB_MAJOR_MYSTERY;i++) {
+			myCards.add(new Card(i,Data.MAJOR_MYSTERY[i]));
+		}
+		
+		//System.out.println(myDeck);
+		
+		myDeck.removeCard(3);
+		
+		System.out.println(myDeck.getDeckOfCards());
+		
 	}
 }
