c95c68f2d5dd2e30586447b2dce82c3900cb7d56
Stephanie <stephanie.perafanpatino@gmail.com>
2020-11-24 13:24:13 +0100

    Test deck methods

---
+++ Tarot/src/Tests/TestDeck.java
@@ -0,0 +1,45 @@
+/**
+ * Test Deck class
+ */
+package Tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
+
+import Model.Card;
+import Model.Data;
+import Model.Deck;
+
+/**
+ * @author Stephanie PERAFAN
+ *
+ */
+class TestDeck {
+	
+	ArrayList testDeck;
+	Deck d;
+	
+	public TestDeck() {
+		testDeck = new ArrayList<Card>();
+		for(int i=0;i<Data.NB_MAJOR_MYSTERY;i++) {
+			testDeck.add(new Card(i,Data.MAJOR_MYSTERY[i]));
+		}
+		
+		d = new Deck(testDeck);
+	}
+
+	@Test
+	void testRemoveCard() {
+		d.removeCard(3);
+		assertEquals(21,d.getDeckOfCards().size());
+	}
+	
+	@Test
+	public void testGetDeckOfCards() {
+		assertEquals(testDeck,d.getDeckOfCards());
+	}
+
+}
