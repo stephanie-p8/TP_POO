2d220271d726cad33525638d63567f7472b9ef7d
Stephanie <stephanie.perafanpatino@gmail.com>
2020-11-26 13:18:55 +0100

    add description for each major mystery card

--- Tarot/src/Model/Data.java
+++ Tarot/src/Model/Data.java
@@ -9,4 +9,13 @@ public class Data {
 			"Le Soleil","Le Jugement","Le Monde"};
 	
 	public static final int NB_MAJOR_MYSTERY = 22;
+	
+	public static final String[] MAJOR_MYSTERY_DESC = {"Fin définitive, inconnu, fin de cycle, départ, fragilité.","Commencement, potentiel, outil, un homme.","Évolution secrète, silence, méditation, une femme.",
+			"Papier, cours, idée, projet, élaboration mentale.","Travail, base solide, construire.","Valeurs, vœux, légalisation, orientation, rectification.",
+			"Carrefour, choix, multiplicité, plaisir.","Progression, bonne nouvelle, un moyen de transport.","Ordre, loi, processus administratif, normalisation.",
+			"Temps, économie, recherche, solitude.","Mouvement, opportunité, changement, voyage.","Énergie, entreprise, maitrise, réalisation.",
+			"Sacrifice, contrainte, dévouement, liens.","Transformation radicale, fin, obligation de couper.","Renaissance, communication, vacances, téléphone.",
+			"Passion, désir, argent, luxure.","Défi, sommet, rapidité, réussite rapide.","Amitiés, douceur, rêve, espoir, protection, lenteur.",
+			"Gestation, obscurité, imagination, famille, intériorité","Chaleur, harmonie, émotion, protection, argent.","Annonce, nouvelle, réussite, libération, rapidité.",
+			"Réussite totale, apothéose, finalité."};
 }
