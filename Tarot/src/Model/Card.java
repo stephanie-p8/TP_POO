b42c45cfd6a74b1e79325ccf23288c75643ec931
Stephanie <stephanie.perafanpatino@gmail.com>
2020-11-26 12:33:34 +0100

    Method to add an image and to update a card

--- Tarot/src/Model/Card.java
+++ Tarot/src/Model/Card.java
@@ -1,9 +1,12 @@
 package Model;
 
+import javax.swing.ImageIcon;
+
 public class Card {
 	private int number;
 	private String name;
 	private String description;
+	private ImageIcon image;
 	
 	public Card(int number,String name) {
 		this.number=number;
@@ -13,6 +16,15 @@ public class Card {
 	public void addDescription(String description) {
 		this.description = description;
 	}
+	
+	public void addImage(ImageIcon image){
+        this.image=image;
+    }
+	
+	public void updateCard(int number, String name){
+        this.setCardNumber(number);
+        this.setCardName(name);
+    }
 	
 	public int getCardNumber() {
 		return number;
@@ -25,6 +37,14 @@ public class Card {
 	public String getDescription() {
 		return description;
 	}
+	
+	public void setCardNumber(int number){
+        this.number=number;
+    }
+
+    public void setCardName(String name){
+        this.name=name;
+    }
 	
 	public String toString() {
 		return "My card is the " + this.number + " whose name is " + this.name + "\n (Description): " + this.description + "\n"; 
