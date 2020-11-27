38f3407e99b03ab534e815183b0625790b26cef9
Stephanie <stephanie.perafanpatino@gmail.com>
2020-11-27 11:45:04 +0100

    test method addImage

--- Tarot/src/Tests/TestCard.java
+++ Tarot/src/Tests/TestCard.java
@@ -3,11 +3,15 @@ package Tests;
 import static org.junit.Assert.*;
 import static org.junit.Assert.assertEquals;
 
+import java.io.File;
+
+import javax.swing.ImageIcon;
 
 import org.junit.jupiter.api.Test;
 
 
 import Model.Card;
+import Model.Data;
 
 public class TestCard {
 	
@@ -22,6 +26,19 @@ public class TestCard {
 		c.addDescription("Commencement, potentiel, outil, un homme");
 		assertTrue(c.getDescription()!=null);
 		
+	}
+	
+	@Test
+	public void testAddImage() {
+		c.addImage(new ImageIcon("images" + File.separator + Data.IMAGES[1]));
+		assertTrue(c.getCardImage()!=null);
+	}
+	
+	@Test
+	public void testUpdateCard() {
+		c.updateCard(3, "La Lune");
+		assertTrue(c.getCardNumber()==3);
+		assertTrue(c.getCardName().equals("La Lune"));
 	}
 	
 	@Test
