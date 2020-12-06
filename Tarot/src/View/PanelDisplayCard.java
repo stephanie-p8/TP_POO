d7cb8360da98fb60d7bd04a0c6d272e1fd04def5
Stephanie <stephanie.perafanpatino@gmail.com>
2020-12-06 19:06:21 +0100

    add button

--- Tarot/src/View/PanelDisplayCard.java
+++ Tarot/src/View/PanelDisplayCard.java
@@ -5,11 +5,14 @@ import javax.swing.*;
 import Model.Data;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class PanelDisplayCard extends JPanel {
+public class PanelDisplayCard extends JPanel implements ActionListener {
 	
 	JLabel labelCard = new JLabel("Choisissez la carte à afficher");
-	JComboBox cards = new JComboBox() ;
+	JComboBox cards = new JComboBox();
+	JButton btnDisplay = new JButton("Afficher");
 	
 	JPanel panelNorth = new JPanel();
 	JPanel panelCenter = new JPanel();
@@ -24,12 +27,27 @@ public class PanelDisplayCard extends JPanel {
 		
 		panelNorth.add(labelCard);
 		panelNorth.add(cards);
+		btnDisplay.addActionListener(this);
+		btnDisplay.setActionCommand("Afficher");
+		panelNorth.add(btnDisplay);
+		
 		
 		this.add(panelNorth,BorderLayout.NORTH);
+		this.add(panelCenter,BorderLayout.CENTER);
 		
-		for(int j=0;j<Data.MAJOR_MYSTERY.length;j++) 
-		if(cards.getSelectedIndex()==j) {
-			
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent event) {
+		// TODO Auto-generated method stub
+		if(event.getActionCommand().equals("Afficher")) {
+			for(int j=0;j<Data.MAJOR_MYSTERY.length;j++) {
+				if(cards.getSelectedIndex()==j) {
+					JLabel label = new JLabel(new ImageIcon(Data.IMAGES[j]));
+					panelCenter.add(label);
+					//this.add(panelCenter,BorderLayout.CENTER);
+				}
+			}
 		}
 		
 	}
